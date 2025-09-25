fc775e6ff72252de25142c258d03ff8a3f859957
powerpaz <61800125+powerpaz@users.noreply.github.com>
2025-09-25 08:24:45 -0500

    Add files via upload

---
+++ config.example.js
@@ -0,0 +1,5 @@
+// Copia este archivo como 'config.js' y rellena con tus claves públicas anon (NO las de servicio)
+window.env = {
+  SUPABASE_URL: "https://<tu_proyecto>.supabase.co",
+  SUPABASE_KEY: "<tu_anon_public_key>"
+};
