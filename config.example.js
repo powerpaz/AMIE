b7359af2ed31a15bbe75055d8c0630449f825f8c
powerpaz <61800125+powerpaz@users.noreply.github.com>
2025-10-06 15:06:17 -0500

    Delete config.example.js

--- config.example.js
+++
@@ -1,5 +0,0 @@
-// Copia como config.js y coloca tus claves públicas anon (Supabase)
-// window.env = {
-//   SUPABASE_URL: "https://<tu_proyecto>.supabase.co",
-//   SUPABASE_KEY: "<tu_anon_public_key>"
-// };
